ce4d7c050ed9cc5583b1d26a62d0a8a5edf0864c
djeck <djeck.det@gmail.com>
2019-04-20 16:22:22 +0200

    Load ram content from file
    And change default parameter to correspond to spartan6 ram macro

--- ram/ram.v
+++ ram/ram.v
@@ -6,8 +6,8 @@ module ram (
 	OE
 	);
 
-parameter AddressSize = 1;
-parameter WordSize = 1;
+parameter AddressSize = 11;
+parameter WordSize = 9;
 
 input [AddressSize-1:0] Address;
 inout [WordSize-1:0] Data;
@@ -16,6 +16,11 @@ input CS, WE, OE;
 reg [WordSize-1:0] Mem [0:(1<<AddressSize)-1];
 
 assign Data = (!CS && !OE) ? Mem[Address] : {WordSize{1'bz}};
+
+initial begin
+	$display("Loading rom.");
+	$readmemb("rom_image.mem", Mem);
+end
 
 always @(CS or WE)
 	if (!CS && !WE)
