187644cf95b54a8290745ac696f9500eb666dbdb
Phileas Fogg <phileas.fogg@mail.com>
2019-07-28 09:39:30 +0200

    Put test's functions in python test_framework
    Make load_test (load values in register) works

--- simpleCPU/tests/add_test.py
+++ simpleCPU/tests/add_test.py
@@ -6,95 +6,21 @@
 #
 
 import sys
-
 # sys.argv[0]: Script name
 # sys.argv[1]: Assembly file
 # sys.argv[2]: Log file
 
-# Use this function to check the design
-# Arguments:
-# - dic_step: contains dictionnary with values of
-#   r0...r7 FE FG FL C and PC registers
-# Return:
-# - True if et matches the expected values,
-#   Else False
-non_reset_pc = "ffff"
-non_reset_pc_int = 0xffff
-
-def load_csv(filename):
-    """
-    Load a csv file containning expected result
-
-    it contains:
-    r0, ..., r7, FE, FG, FL, C
-
-    values are either 1 byte hexadecimals or xx for
-    don't care vales
-    """
-    output_list = []
-    with open(filename) as resfile:
-        first_line = resfile.readline().strip()
-        keys = first_line.split(",")
-        line_count = 0
-        for line in resfile:
-            line_count += 1
-            splitted = line.strip().split(",")
-            if splitted==['']:
-                continue
-            elif len(splitted) != len(keys):
-                print("Error in CSV file %s: %s" % (filename, splitted))
-                print("splitted is %d, keys are %d" % (len(splitted),len(keys)))
-                continue
-            line_dic = {}
-            for i in range(len(splitted)):
-                line_dic[keys[i]] = splitted[i];
-            output_list.append(line_dic)
-
-    return output_list[:]
-
-def check_line(result_dic, pattern_dic, verbose=False):
-    for key in pattern_dic:
-        if pattern_dic[key] == "xx":
-            continue
-        elif pattern_dic[key][0] == 'z' or result_dic[key][0] == 'z':
-            if pattern_dic[key] != result_dic[key]:
-                return False
-            else:
-                continue
-        elif key in result_dic and int(result_dic[key], 16) != int(pattern_dic[key], 16):
-            if verbose:
-                print("%s Error: %s does not match %s" % (key, pattern_dic[key],result_dic[key]))
-            return False
-    return True
-
-def check_all(array_all, expected):
-    if len(array_all) < len(expected):
-        print("Error: Output is too short to match expected results")
-        return False
-    item_i = 0
-    while item_i < len(array_all):
-        if check_line(array_all[item_i], expected[0]):
-            break
-        item_i += 1
-    print("Detected offset: %d / %d" % (item_i, len(array_all)))
-    if len(array_all)-item_i < len(expected):
-        print("Error: Output offset to big...")
-        return False
-    for index in range(len(expected)):
-        if not check_line(array_all[item_i+index], expected[index]):
-            print("Error at index %d: PC=%s" % (index, array_all[item_i+index]["PC"]))
-            return False
-    return True
+import test_framework
 
 if len(sys.argv) != 3:
     print("Error: Expect 3 arguments")
     print("Look at utils/launch_tests.sh for more information")
     exit(1) # Error
 
-expected = load_csv("tests/add_test.csv")
-array_all = load_csv(sys.argv[2])
+expected = test_framework.load_csv("tests/add_test.csv")
+array_all = test_framework.load_csv(sys.argv[2])
 
-if not check_all(array_all, expected):
+if not test_framework.check_all(array_all, expected):
     exit(1) # Do not match expected values
 else:
     exit(0) # Success
