fb886449a0ba4b4792f90db7970caf2de5137a39
Phileas Fogg <phileas.fogg@mail.com>
2019-07-30 19:18:35 +0200

    Functionnality check: OR
    Add functional test for the or operation

---
+++ simpleCPU/tests/or_test.py
@@ -0,0 +1,27 @@
+#!/bin/python2
+
+#
+# This script is called by utils/launch_test.sh
+# it exit(0) when success else exit(0)
+#
+
+import sys
+# sys.argv[0]: Script name
+# sys.argv[1]: Assembly file
+# sys.argv[2]: Log file
+
+import test_framework
+
+if len(sys.argv) != 3:
+    print("Error: Expect 3 arguments")
+    print("Look at utils/launch_tests.sh for more information")
+    exit(1) # Error
+
+expected = test_framework.load_csv("tests/or_test.csv")
+array_all = test_framework.load_csv(sys.argv[2])
+
+if not test_framework.check_all(array_all, expected):
+    exit(1) # Do not match expected values
+else:
+    exit(0) # Success
+
