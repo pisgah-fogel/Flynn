f41151186ca4b76ff1baec802c1045e7eea70ae3
Phileas Fogg <phileas.fogg@mail.com>
2019-10-19 22:45:14 +0200

    Add traces
    Traces can be openned with GtkWave.
    Tested Ok

--- verilator_test/03-Blink/main.cpp
+++ verilator_test/03-Blink/main.cpp
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include "obj_dir/Vblink.h"
 #include <verilated.h>
+#include <verilated_vcd_c.h>
 
-#define DURATION (2000000)
-#define CLK_PER_SEC (2000000)
+#define DURATION (100000)
+#define CLK_PER_SEC (1000000)
 
 #define ACTIVATE_LOGGING 0
 #define LOG (1000)
@@ -12,10 +13,17 @@
 static unsigned long int ticks = 0;
 static unsigned long int last_log = 0;
 
-void tick(Vblink *tb) {
+void tick(Vblink *tb, VerilatedVcdC *tfp) {
 	tb->eval();
+	if (tfp)
+		tfp->dump(ticks);
+	ticks ++;
 	tb->i_clk = 0;
 	tb->eval();
+	if (tfp) {
+		tfp->dump(ticks);
+		tfp->flush();
+	}
 	ticks ++;
 	tb->i_clk = 1;
 	tb->eval(); // return on rising edge
@@ -28,9 +36,15 @@ int main (int argc, char **argv)
 
 	Vblink *tb = new Vblink;
 
+	// Generate a trace
+	Verilated::traceEverOn(true);
+	VerilatedVcdC* tfp = new VerilatedVcdC;
+	tb->trace(tfp, 99);
+	tfp->open("blinktrace.vcd");
+
 	for (int k=0; k<DURATION; k++) {
 
-		tick(tb);
+		tick(tb, tfp);
 #if ACTIVATE_LOGGING
 		if ((ticks % LOG == 0) || (last_led != tb->o_led)) {
 			last_led = tb->o_led;
