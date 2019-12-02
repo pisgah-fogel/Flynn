1bae3e581b3ee99d39e0abf2d1f010ce717a2126
Phileas Fogg <phileas.fogg@mail.com>
2019-12-02 21:22:09 +0100

    WIP, 18 Warnings, create Makefile for verilator

---
+++ UART/Verif/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "obj_dir/Vreset_sync.h"
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+
+int main (int argc, char **argv)
+{
+	printf("Work in progress\n");
+	printf("Nothing to do\n");
+	return 0;
+}
