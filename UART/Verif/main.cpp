7407ac5b6ab752a2b943401411cd98f5e5dbdf0f
Aubin Detrez <aubin.detrez@ibm.com>
2019-12-04 16:24:14 +0100

    Correct including error

--- UART/Verif/main.cpp
+++ UART/Verif/main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "obj_dir/Vreset_sync.h"
+#include "obj_dir/Vtop_module.h"
 #include <verilated.h>
 #include <verilated_vcd_c.h>
 
