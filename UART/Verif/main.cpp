dec5d7f1c84bfab2e63d7b7b743e922c8c8d05e2
Phileas Fogg <phileas.fogg@mail.com>
2019-12-05 21:48:01 +0100

    WIP Add wait_tick function

--- UART/Verif/main.cpp
+++ UART/Verif/main.cpp
@@ -5,6 +5,32 @@
 #include <verilated.h>
 #include <verilated_vcd_c.h>
 
+#define TICKS_PER_SEC 100000000 // 100MHz clocks
+
+static unsigned long int ticks = 0;
+static unsigned long int errors = 0;
+static Vtop_module *tb;
+static VerilatedVcdC* tfp;
+
+void wait_cycle(unsigned int nb) {
+	unsigned int i;
+	for(i=0; i<nb; i++) {
+		tb->eval();
+		if (tfp)
+			tfp->dump(ticks);
+		ticks ++;
+		tb->i_clk = 0;
+		tb->eval();
+		if (tfp) {
+			tfp->dump(ticks);
+			tfp->flush();
+		}
+		ticks ++;
+		tb->i_clk = 1;
+		tb->eval(); // return on rising edge
+	}
+}
+
 int main (int argc, char **argv)
 {
 	printf("Work in progress\n");
