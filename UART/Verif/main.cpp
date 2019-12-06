047a748a4e196eb3e473f1760bea1bc38829ead4
Phileas Fogg <phileas.fogg@mail.com>
2019-12-06 21:14:08 +0100

    WIP Receive UART

--- UART/Verif/main.cpp
+++ UART/Verif/main.cpp
@@ -12,20 +12,69 @@ static unsigned long int errors = 0;
 static Vtop_module *tb;
 static VerilatedVcdC* tfp;
 
+
+static bool uart_read_is_recv = false;
+static unsigned long int uart_read_count= 0;
+static unsigned long int uart_read_half_bit= 0;
+static unsigned int uart_read_data= 0;
+void uart_read() {
+	const unsigned int clock_freq = 100000000; // 100MHz
+	const unsigned int baud_rate = 115200;
+	const unsigned int half_bit_period_cycles = clock_freq/baud_rate;
+
+	if (!uart_read_is_recv) {
+		if (tb->Tx == 0) {
+			uart_read_is_recv = true;
+			uart_read_count = 0;
+			uart_read_data = 0;
+			uart_read_half_bit = 0;
+			printf("Tx = %d\n", tb->Tx);
+			printf("UART: Start bit received (tick %ld)\n", ticks);
+		}
+	} else {
+		uart_read_count++;
+		if (uart_read_count == half_bit_period_cycles) {
+			uart_read_count= 0;
+			printf("Tx = %d\n", tb->Tx);
+			printf("UART: Half bit n %ld (%ld)\n", uart_read_half_bit, ticks);
+			if (uart_read_half_bit == 2*1 + 2*8 + 1*2) {
+				printf("UART: End of reception n (%ld)\n", ticks);
+				uart_read_is_recv = false;
+			}
+			uart_read_half_bit++;
+		} else if (uart_read_half_bit == 0) {
+			if (tb->Tx != 0) {
+				printf("Tx = %d\n", tb->Tx);
+				printf("Error tick %ld : Start bit should be 0\n", ticks);
+				errors++;
+				uart_read_is_recv = false;
+			}
+		} else if (uart_read_half_bit == 2*1 + 2*8 + 1*2) {
+			if (tb->Tx != 1) {
+				printf("Tx = %d\n", tb->Tx);
+				printf("Error tick %ld : End bit should be 1\n", ticks);
+				errors++;
+			}
+		}
+	}
+	
+}
+
 void wait_cycle(unsigned int nb) {
 	unsigned int i;
 	for(i=0; i<nb; i++) {
 		tb->eval();
+		uart_read();
 		if (tfp)
 			tfp->dump(ticks);
 		ticks ++;
 		tb->clk = 0;
 		tb->eval();
+		uart_read();
 		if (tfp) {
 			tfp->dump(ticks);
 			tfp->flush();
 		}
-		ticks ++;
 		tb->clk = 1;
 		tb->eval(); // return on rising edge
 	}
@@ -38,7 +87,6 @@ void wait_ms(unsigned int nb) {
 void uart_write(const unsigned int data) {
 	const unsigned int clock_freq = 100000000; // 100MHz
 	const unsigned int baud_rate = 115200;
-
 	const unsigned int bit_period_cycles = clock_freq/baud_rate;
 
 	unsigned int buffer = data;
@@ -76,7 +124,7 @@ int main (int argc, char **argv)
 	tb->clk = 0;
 	tb->btn = 0b00001111 & 0;
 	tb->Rx = 1;
-	tb->sw = 0b01111111 & 0;
+	tb->sw = 0b01111111 & 0b00000010;
 
 	printf("Wait 1 ms\n");
 	wait_ms(1);
