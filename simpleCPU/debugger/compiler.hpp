1403a7d7a8e2326011f365b9d289d75f8d877968
Phileas Fogg <phileas.fogg@mail.com>
2020-10-12 19:40:05 +0200

    Adding append_address

--- simpleCPU/debugger/compiler.hpp
+++ simpleCPU/debugger/compiler.hpp
@@ -10,9 +10,21 @@ bool is_bin(const std::string &str) {
     return true;
 }
 
+void append_address(std::string* output, const std::string &addr_str) {
+    if (addr_str.size() != 8) {
+        std::cout<<"Compiler Error: "<<addr_str<<" is not a valid address (8 bits expected)."<<std::endl;
+        return;
+    }
+    if (! is_bin(addr_str)) {
+        std::cout<<"Compiler Error: address value "<<addr_str<<" is not a valid binary number."<<std::endl;
+        return;
+    }
+    output->append(addr_str);
+}
+
 void append_register_as_binary(std::string* output, const std::string &str) {
     if (str.length() != 2 || str[0] != 'r') {
-        std::cout<<"Compiler Error: Wrong register: "<<str<<" r0, r1... expected"<<std::endl;
+        std::cout<<"Compiler Error: Wrong register: "<<str<<" r0, r1... expected."<<std::endl;
         return;
     }
     if (str[1] == '0')
@@ -32,7 +44,7 @@ void append_register_as_binary(std::string* output, const std::string &str) {
     else if (str[1] == '7')
         output->append("111");
     else
-        std::cout<<"Compiler Error: Wrong register: "<<str<<" r0, r1... expected"<<std::endl;
+        std::cout<<"Compiler Error: Wrong register: "<<str<<" r0, r1... expected."<<std::endl;
 }
 
 #ifdef UNIT_TEST
@@ -60,6 +72,7 @@ BOOST_AUTO_TEST_CASE( tc_int_to_binary_string )
   BOOST_TEST_REQUIRE( "00000001" == int_to_binary_string(1, 8) );
   BOOST_TEST_REQUIRE( "11111111" == int_to_binary_string(255, 8) );
   BOOST_TEST_REQUIRE( "11011001" == int_to_binary_string(0b11011001, 8) );
+  BOOST_TEST_REQUIRE( "" == int_to_binary_string(43, 0) );
 }
 
 BOOST_AUTO_TEST_CASE( tc_append_register_as_binary ) // tc = Testcase
@@ -81,6 +94,33 @@ BOOST_AUTO_TEST_CASE( tc_is_bin )
     BOOST_TEST_REQUIRE( is_bin("00101210101") == false );
     BOOST_TEST_REQUIRE( is_bin("I0101010101") == false );
     BOOST_TEST_REQUIRE( is_bin("1010101010b") == false );
+    BOOST_TEST_REQUIRE( is_bin("") == true );
+}
+
+BOOST_AUTO_TEST_CASE( tc_append_address )
+{
+    std::string output;
+
+    append_address(&output, "01110101");
+    BOOST_TEST_REQUIRE( output == "01110101" );
+    output.clear();
+
+    std::cout<<"Please ignore compiler's errors:"<<std::endl;
+    append_address(&output, "0111010a");
+    BOOST_TEST_REQUIRE( output == "" );
+    output.clear();
+
+    append_address(&output, "01110102");
+    BOOST_TEST_REQUIRE( output == "" );
+    output.clear();
+
+    append_address(&output, "a1110101");
+    BOOST_TEST_REQUIRE( output == "" );
+    output.clear();
+
+    append_address(&output, "21110101");
+    BOOST_TEST_REQUIRE( output == "" );
+    output.clear();
 }
 
 BOOST_AUTO_TEST_SUITE_END()
