e9a2262eecdf9e96457a58a8759e241bea13b472
Phileas Fogg <phileas.fogg@mail.com>
2021-07-11 16:27:43 +0200

    Blinking led and sync reset

--- colorLight/button.v
+++ colorLight/button.v
@@ -1,10 +1,20 @@
 module button (
-	input i_clk,
-	input i_btn,
-	output reg o_led
+	input wire i_clk,
+	input wire i_btn,
+	output wire o_led
 	);
-	always @(posedge i_clk)
+
+	reg [24:0] r_counter;
+	initial
+		r_counter <= 0;
+
+	assign o_led = !r_counter[24];
+
+	always @(posedge i_clk) // or negedge i_btn
 	begin
-		o_led <= i_btn;
+		if (i_btn == 1'b0)
+			r_counter <= 0;
+		else
+			r_counter <= r_counter + 1;
 	end
 endmodule
